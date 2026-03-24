b39a59253d9247d9570dbedfd3c240320e8bad83
KoFeVarKaA <155472727+KoFeVarKaA@users.noreply.github.com>
2026-03-24 12:14:43 +0300

    delete tips

--- siteweather/weather/models.py
+++ siteweather/weather/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Locations(models.Model):
+    """
+    name - Название
+    user_id - Пользователь, добавивший эту локацию
+    latitude - Широта локации
+    longitude - Долгота локации
+    """
+    name = models.CharField()
+    user_id = models.ManyToManyField(User, on_delete=models.CASCADE)
+    latitude = models.DecimalField()
+    longitude = models.DecimalField()
+
+    def __str__(self):
+        return self.name
--- siteweather/weather/urls.py
+++ siteweather/weather/urls.py
@@ -3,19 +3,6 @@ from django.urls import path, re_path, register_converter
 from .views import WeatherViews
 from .converters import FourDigitYearConverter
 
-# Регистрируем кастомный конвертер
-register_converter(FourDigitYearConverter, "year4")
-
 urlpatterns = [
-    path('', WeatherViews.index, name='home'), # Главная страница
-    path('get_exl', WeatherViews.get_example, name='get_example'),
-    # path('/smthg/<int:id>/', WeatherViews.something), # С query и конвертером
-    # path('/smthg/<slug:text>/', WeatherViews.something), # Если не сработает int, отработает slug
-
-    # re_path(r'^archive/(?P<year>[0-9]{4})/', WeatherViews.archive)  
-        # Работает с регулярными выражениями, если невозможно описать с исп. path
-        # /archive/2023/ year=2023, должен состоять из цифр и быть из 4 символов 
-        
-    # Можно определять свои конверторы
-    # path('archive/<year4: year>/', WeatherViews.archive),
+    
 ]
