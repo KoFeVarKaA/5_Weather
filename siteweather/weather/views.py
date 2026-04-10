beb7b3448ced0a53be16804f0d9fa2a87a80607f
KoFeVarKaA <155472727+KoFeVarKaA@users.noreply.github.com>
2026-04-10 17:51:15 +0300

    update templates

--- siteweather/weather/views.py
+++ siteweather/weather/views.py
@@ -2,24 +2,3 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 
 # Create your views here.
-class WeatherViews():
-    # Представление в виде функции
-    def index(request: HttpRequest):     #HttpRequest, должна принимать один параметр
-        return HttpResponse("Главная страница weather") # Возвращает ответ(str)
-    
-    def get_example(request: HttpRequest):
-        query = request.GET # Здесь будет все query в формате dict
-        return HttpResponse(f"Query: {query}") # Возвращает ответ(str)
-
-    # обработка шаблонного пути
-    # def something(request: HttpRequest, id):
-    #     return HttpResponse(f"Path template id: {id}")
-
-    # Пути с re_path
-    # def archive(request: HttpRequest, year):
-    #     return HttpResponse(f"C re_path {year}")
-
-    # Redirect
-    # def something(request: HttpRequest):
-    #     return redirect('get_example') #Можно указывать name из path
-    #     return HttpResponseRedirect('/')
